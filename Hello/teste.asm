6241aeaefb520b3cb1060b4e19b8722d8919eed5
Petrucio Neves <petrucio-filho@hotmail.com>
2021-10-16 18:11:49 -0300

    Hello World

--- Hello/teste.asm
+++ Hello/teste.asm
@@ -1,19 +1,21 @@
-section .data
-	msg db 'Hello World!', 0xA
-	tam equ $-msg
+;Código "Hello Wordl!" em assembly
 
-section .text
+section .data						;Secao de variaveis
+	msg db 'Hello World!', 0xA		;Mensagem que sera mostrada
+	tam equ $-msg					;Quantidade de caracteres da mensagem
+
+section .text						;Secao do Programa
 
 global _start
 
-_start:
-	mov eax, 0x4
-	mov ebx, 0x1
-	mov ecx, msg
-	mov edx, tam
-	int 0x80
+_start:								;Marcador inicial
+	mov eax, 0x4					;"0x4" movido para eax, indica que se trata de uma saida
+	mov ebx, 0x1					;"0x1" movido para ebx, indica que será realizado no terminal
+	mov ecx, msg					;Passa mensagem que será mostrada, no ecx
+	mov edx, tam					;passa a quantidade de caracteres para o edx
+	int 0x80						;Envia a informacao ao Sistema Operacional
 	
-saida:
-	mov eax, 0x1
-	mov ebx, 0x0
-	int 0x80
+saida:								;Marcador de saida
+	mov eax, 0x1					;Informa o termino das acoes
+	mov ebx, 0x0					;informa o estado final do programa - 0 que é sem erro
+	int 0x80						;Envia a informacao ao Sistema Operacional
